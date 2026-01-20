7aed08d7594fc401fbc0a07d757e8508fba071ae
checklink-app <chrisvasileiou73@gmail.com>
2026-01-20 22:34:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -154,9 +154,3 @@ def check():
     </html>
     """
 
-# =========================
-# RUN (Railway compatible)
-# =========================
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
